b70dd11f659b581f00238de9e32fe50367b300e3
Manuvaldi <manuvaldi@gmail.com>
2017-06-13 09:30:58 +0200

    Avoid duplicate image creation when redeploy (#156)
    * Avoid duplicate image creation when redeploy
    
    When you run "enos deploy" in a cluster already deployed. This change avoids "cirrus.uec" image is created again and show duplicate in Openstack.
    
    * Fix too long line build error
    
    * Variable substitution bad syntax in image creation fixed

--- enos/enos.py
+++ enos/enos.py
@@ -258,13 +258,16 @@ def init_os(env=None, **kwargs):
                'url': url}]
     for image in images:
         cmd.append("wget -q -O /tmp/%s %s" % (image['name'], image['url']))
-        cmd.append("openstack image create"
+        cmd.append("openstack image list "
+                   "--property name=%(image_name)s -c Name -f value "
+                   "| grep %(image_name)s"
+                   "|| openstack image create"
                    " --disk-format=qcow2"
                    " --container-format=bare"
                    " --property architecture=x86_64"
                    " --public"
-                   " --file /tmp/%s"
-                   " %s" % (image['name'], image['name']))
+                   " --file /tmp/%(image_name)s"
+                   " %(image_name)s" % {'image_name': image['name'], })
 
     # flavors name, ram, disk, vcpus
 
