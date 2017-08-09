9cb9a0edb6102c91498e6cb5ecf81311b34c4fba
msimonin <matthieu.simonin@inria.fr>
2017-08-09 12:07:53 +0200

    Fix empty path to workload in sanity_check
    The variable BASE_DIR was unset for some tests.
    Make sure it is set by passing it as argument to sanity check.
    This way files in current tree can be use in the tests.
    
    Additionnaly a pass of shellcheck has been made
    
    Cherry-picked from 91d0e6330b1e9024a8ba24c32f428b980c8dc2e8

--- tests/functionnal/tests/grid5000/deploy.sh
+++ tests/functionnal/tests/grid5000/deploy.sh
@@ -3,20 +3,23 @@
 set -xe
 
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-BASE_DIR=../../../..
+BASE_DIR="${SCRIPT_DIR}/../../../.."
 
-cd $SCRIPT_DIR
+cd "$SCRIPT_DIR"
 
+
+# shellcheck disable=SC1091
 . ../utils.sh
 
 virtualenv venv
+# shellcheck disable=SC1091
 . venv/bin/activate
 
-pip install -e $BASE_DIR
+pip install -e "$BASE_DIR"
 
 echo "-ENOS DEPLOY-"
 enos deploy -f grid5000.yaml
-sanity_check
+sanity_check "$BASE_DIR"
 echo "-ENOS DESTROY-"
 enos destroy
 
