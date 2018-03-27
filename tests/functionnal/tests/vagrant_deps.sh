9ba23329cd2b228f9fc99fb1bb6ebc1de9715c26
msimonin <matthieu.simonin@inria.fr>
2018-03-27 20:27:58 +0200

    Fix packaging ci job

--- tests/functionnal/tests/vagrant_deps.sh
+++ tests/functionnal/tests/vagrant_deps.sh
@@ -4,11 +4,11 @@ set -xe
 
 apt-get -y update
 # install vagrant
-wget https://releases.hashicorp.com/vagrant/1.9.6/vagrant_1.9.6_x86_64.deb -O vagrant.deb
+wget https://releases.hashicorp.com/vagrant/2.0.3/vagrant_2.0.3_x86_64.deb -O vagrant.deb
 dpkg -i vagrant.deb
 apt-get -f -y  install
 # install vbox
-wget http://download.virtualbox.org/virtualbox/5.1.22/virtualbox-5.1_5.1.22-115126~Debian~jessie_amd64.deb -O vbox.deb
+wget https://download.virtualbox.org/virtualbox/5.2.8/virtualbox-5.2_5.2.8-121009~Debian~jessie_amd64.deb -O vbox.deb
 dpkg -i vbox.deb || true
 apt-get -f -y  install
 
