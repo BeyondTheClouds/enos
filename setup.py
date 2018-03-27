4d10e438489d2806a5d73c47a0da9613acccd508
msimonin <matthieu.simonin@inria.fr>
2018-03-27 14:17:35 +0200

    Fix chameleon provider dependency
    * We use a deprecated API (add_floating_ip).
      For now I choose to force the novaclient to a old version
    * dateutils is incompatible fix the one used by blazar
    
    Cherry-pick from f3f1a9bc98b9e1c430041981aecb50c6e7f59359

--- setup.py
+++ setup.py
@@ -35,12 +35,17 @@ setup(
         'influxdb==4.0.0',
         'docopt>=0.6.2,<0.7.0',
         'httplib2==0.9.2',
-        'python-dateutil==2.2',
+        'python-dateutil>=2.2',
         'python-openstackclient>=3.0.0,<=4.0.0',
         'python-neutronclient==6.3.0',
         'python-vagrant>=0.5.14,<0.6.0',
-        # 'python-blazarclient==0.2.0',
-        # ReadTheDocs
+        # - Chameleon
+        # NOTE(msimonin): chameleon provider is using
+        # a deprecated API (add_floating_ip).
+        'python-novaclient<10',
+        'python-blazarclient==1.0.1',
+        'python-neutronclient==6.3.0',
+        # - ReadTheDocs
         'GitPython>=2.1.5'
     ],
     entry_points={'console_scripts': ['enos = enos.enos:main']},
