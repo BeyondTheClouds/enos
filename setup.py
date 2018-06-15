3748c7c4db69a87554d2d8c8d2267dc39dd6b799
msimonin <matthieu.simonin@inria.fr>
2018-06-15 12:31:38 +0200

    unfreeze cmd2
    This has been handled upstream for
    - stable/ocata: https://review.openstack.org/#/c/571260
    - stable/pike: https://review.openstack.org/#/c/571105
    - stable/queens: https://review.openstack.org/#/c/571259/
    - master: https://review.openstack.org/#/c/570811
    
    Cherry-pick from: e92a46ffddebf6d8276df4ab5b0c4c884713b98c

--- setup.py
+++ setup.py
@@ -46,8 +46,6 @@ setup(
         'python-neutronclient==6.3.0',
         # - ReadTheDocs
         'GitPython>=2.1.5',
-        # force python 2.7
-        'cmd2<0.9.0'
     ],
     entry_points={'console_scripts': ['enos = enos.enos:main']},
     include_package_data=True
