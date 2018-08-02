88a6506a05f8428be27ac6e247f80440f30cc9ae
jrbalderrama <jrbalderrama@users.noreply.github.com>
2018-08-02 12:53:11 +0200

    fix provision

--- enos/provider/g5k.py
+++ enos/provider/g5k.py
@@ -8,6 +8,13 @@ from enoslib.api import expand_groups
 from provider import Provider
 
 
+import execo as EX
+
+
+# ROLE_DISTRIBUTION_MODE_STRICT = "strict"
+DEFAULT_CONN_PARAMS = {'user': 'root'}
+
+
 logger = logging.getLogger(__name__)
 
 
@@ -58,7 +65,7 @@ def _build_enoslib_conf(conf):
         groups = expand_groups(desc["group"])
         for group in groups:
             machine = {"roles": [group, desc["role"]],
-                       "number": desc["number"],
+                       "nodes": desc["number"],
                        "cluster": desc["flavor"],
                        "primary_network": "int-net"}
 
@@ -75,6 +82,56 @@ def _build_enoslib_conf(conf):
     return enoslib_conf
 
 
+    def _exec_command_on_nodes(nodes, cmd, label, conn_params=None):
+        """Execute a command on a node (id or hostname) or on a set of nodes"""
+        if isinstance(nodes, basestring):
+            nodes = [nodes]
+
+        if conn_params is None:
+            conn_params = DEFAULT_CONN_PARAMS
+
+        logging.info(label)
+        remote = EX.Remote(cmd, nodes, conn_params)
+        remote.run()
+
+        if not remote.finished_ok:
+            raise Exception('An error occcured during remote execution')
+
+    def _provision(roles):
+
+        nodes = set()
+        for value in roles.values():
+            nodes = nodes.union(set(value))
+        
+        # Provision nodes so we can run Ansible on it
+        _exec_command_on_nodes(
+            nodes,
+            'apt-get update && apt-get -y --force-yes install python',
+            'Installing python...')
+
+        # Bind volumes of docker in /tmp (free storage location on G5k)
+        _exec_command_on_nodes(
+            nodes,
+            ('mkdir -p /tmp/docker/volumes; '
+             'mkdir -p /var/lib/docker/volumes'),
+            'Creating docker volumes directory in /tmp')
+
+        _exec_command_on_nodes(
+            nodes,
+            ('(mount | grep /tmp/docker/volumes) || '
+             'mount --bind /tmp/docker/volumes /var/lib/docker/volumes'),
+            'Bind mount')
+
+        # Bind nova local storage in /tmp
+        _exec_command_on_nodes(
+            nodes,
+            'mkdir -p /tmp/nova ; mkdir -p /var/lib/nova',
+            'Creating nova directory in /tmp')
+        _exec_command_on_nodes(
+            nodes,
+            '(mount | grep /tmp/nova) || mount --bind /tmp/nova /var/lib/nova',
+'Bind mount')
+
 class G5k(Provider):
 
     def init(self, conf, force_deploy=False):
@@ -83,7 +140,8 @@ class G5k(Provider):
         logging.debug("Creating G5K provider")
         g5k = enoslib_g5k.G5k(enoslib_conf)
         logging.info("Initializing G5K provider")
-        roles, networks = g5k.init(force_deploy)
+        roles, networks = g5k.init(force_deploy)        
+        _provision(roles)
         return roles, networks
 
     def destroy(self, env):
@@ -98,7 +156,7 @@ class G5k(Provider):
     def default_config(self):
         return {'job_name': 'Enos',
                 'walltime': '02:00:00',
-                'env_name': 'debian9-x64-min',
+                'env_name': 'debian9-x64-nfs',
                 'reservation': False,
                 'role_distribution': ROLE_DISTRIBUTION_MODE_STRICT,
                 'single_interface': False,
