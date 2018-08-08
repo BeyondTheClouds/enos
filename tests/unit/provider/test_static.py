2febc24d3ad0a3501a658b9fdddbb3cf96f92c05
msimonin <matthieu.simonin@inria.fr>
2018-08-08 09:11:21 +0200

    [EnOSlib][static] Provider

--- tests/unit/provider/test_static.py
+++ tests/unit/provider/test_static.py
@@ -1,37 +1,49 @@
-from enos.provider.static import Static
+from enos.provider.static import _build_enoslib_conf
+import operator
 import unittest
 
 
-class TestBuildResources(unittest.TestCase):
-    def test_build_resources(self):
-        def _uniformize(the_dict):
-            "Sorts table values of `the_dict` to make order predictable."
-            return {k: sorted(v) for k, v in the_dict.items()}
+class TestGenEnoslibRoles(unittest.TestCase):
 
-        topology = {
-          "grp1": {
-            "compute": {
-              "address": "ip1"
-            },
-            "control": {
-              "address": "ip2"
-            }
-          },
-          "grp2": {
-            "compute": {
-              "address": "ip3"
-            }
-          }
+    def test_with_resources(self):
+        resources = {
+            "compute": [{"address": "1.2.3.4", "alias": "enos-0"}] ,
+            "control": [{"address": "1.2.3.5", "alias": "enos-1"}] ,
+            "network": [{"address": "1.2.3.6", "alias": "enos-2"}] ,
+        }
+        conf = {
+            "provider": {"networks": []},
+            "resources": resources
+        }
+        enoslib_conf = _build_enoslib_conf(conf)
+        machines = sorted(enoslib_conf["resources"]["machines"],
+                          key=operator.itemgetter("address"))
+        self.assertEqual(["1.2.3.4", "1.2.3.5", "1.2.3.6"],
+                         [m["address"] for m in machines])
+        self.assertEqual(["default_group", "compute"], machines[0]["roles"])
+        self.assertEqual(["default_group", "control"], machines[1]["roles"])
+        self.assertEqual(["default_group", "network"], machines[2]["roles"])
+
+
+    def test_with_topology(self):
+        resources_1 = {
+            "compute": [{"address": "1.2.3.4", "alias": "enos-0"}] ,
+            "control": [{"address": "1.2.3.5", "alias": "enos-1"}] ,
+        }
+        resources_2 = {
+            "network": [{"address": "1.2.3.6", "alias": "enos-2"}] ,
         }
 
-        resources_expected = {
-            "control": [{"address": "ip2"}],
-            "compute": [{"address": "ip1"}, {"address": "ip3"}],
-            "grp1": [{"address": "ip1"}, {"address": "ip2"}],
-            "grp2": [{"address": "ip3"}]
+        conf = {
+            "provider": {"networks": []},
+            "topology": {"grp1": resources_1, "grp2": resources_2}
         }
 
-        resources_actual = Static().topology_to_resources(topology)
-
-        self.assertDictEqual(_uniformize(resources_expected),
-                             _uniformize(resources_actual))
+        enoslib_conf = _build_enoslib_conf(conf)
+        machines = sorted(enoslib_conf["resources"]["machines"],
+                          key=operator.itemgetter("address"))
+        self.assertEqual(["1.2.3.4", "1.2.3.5", "1.2.3.6"],
+                         [m["address"] for m in machines])
+        self.assertEqual(["grp1", "compute"], machines[0]["roles"])
+        self.assertEqual(["grp1", "control"], machines[1]["roles"])
+        self.assertEqual(["grp2", "network"], machines[2]["roles"])
