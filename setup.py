7503c5cba236928b42c83152e3276fc51948f17f
msimonin <matthieu.simonin@inria.fr>
2018-12-17 10:37:38 +0100

    [enoslib_2.0.0] update to latest enoslib alpha

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     long_description=read('README.rst'),
     packages=find_packages(),
     install_requires=[
-        'enoslib==2.0.0-alpha.1',
+        'enoslib==2.0.0-alpha.2',
         'docopt>=0.6.2,<0.7.0',
         # All kolla commands will run inside a dedicated venv. The version is
         # unspecified since the use of virtualenv is very basic.
