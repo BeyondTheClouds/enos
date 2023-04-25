a8e30e9490fd49841f4e6e7299f998bd19d52603
jonglezb <jonglezb@users.noreply.github.com>
2023-04-25 14:29:33 +0000

    deploy: 18d4e318f13e9584c339b4e7197cd22d11cbaba1

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '7.1.1',
+    VERSION: '8.0.0a1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
